0159dbaf444b7f4fbff1bd4c071d497e2ab01c14
Anthony <angarce01@gmail.com>
2024-05-28 12:41:09 -0700

    fix loanActions placement of conditional error message rendering

--- app/components/LoanActions.tsx
+++ app/components/LoanActions.tsx
@@ -35,9 +35,6 @@ type LoanResponse = Loan & {message?: string}
 const LoanActions = ({ params, user }: { params: { id: string }, user: User }) => {
   const queryClient = useQueryClient();
   const loan = queryClient.getQueryData<LoanResponse>(["loan"]);
-  if (loan && 'message' in loan && loan.message === 'User is not a member of the team associated with this loan') {
-    return <ErrorMessage>{loan.message}</ErrorMessage>
-  }
   
   const [editLoading, setEditLoading] = useState(false);
   const [prevStageLoading, setPrevStageLoading] = useState(false);
@@ -54,84 +51,88 @@ const LoanActions = ({ params, user }: { params: { id: string }, user: User }) =
   useState<loansDisplayDataInterface>();
   
   useEffect(() => {
-      // mapping over loansDisplayData, and if an item's .value matches loanStage, dynamically set the color
-      if (loan) {
-          loansDisplayData.map((displayData) => {
-              if (displayData.value === loan.pipelineStage) {
-                  setLoanColor(displayData.color);
-                }
-            });
-            
-            // find the current index with .findIndex, which returns the index of the first element in the array that satisfies the provided testing function
-            setCurrentStageIndex(
-                loansDisplayData.findIndex(
-                    (displayData) => displayData.value === loan?.pipelineStage
-                )
-            );
+    // mapping over loansDisplayData, and if an item's .value matches loanStage, dynamically set the color
+    if (loan) {
+      loansDisplayData.map((displayData) => {
+        if (displayData.value === loan.pipelineStage) {
+          setLoanColor(displayData.color);
         }
-    }, [loan]);
-    
-    // separate useEffect to handle the forward and back buttons
-    useEffect(() => {
-        //logic to prevent forward and back buttons from rendering when there is no next or previous stage
-        if (currentStageIndex !== -1) {
-            if (currentStageIndex < loansDisplayData.length) {
-                setNextStageInfo(loansDisplayData[currentStageIndex + 1]);
-                setPreviousStageInfo(loansDisplayData[currentStageIndex - 1]);
+      });
+      
+      // find the current index with .findIndex, which returns the index of the first element in the array that satisfies the provided testing function
+      setCurrentStageIndex(
+        loansDisplayData.findIndex(
+          (displayData) => displayData.value === loan?.pipelineStage
+        )
+      );
+    }
+  }, [loan]);
+  
+  // separate useEffect to handle the forward and back buttons
+  useEffect(() => {
+    //logic to prevent forward and back buttons from rendering when there is no next or previous stage
+    if (currentStageIndex !== -1) {
+      if (currentStageIndex < loansDisplayData.length) {
+        setNextStageInfo(loansDisplayData[currentStageIndex + 1]);
+        setPreviousStageInfo(loansDisplayData[currentStageIndex - 1]);
             }
-        }
+          }
         
         // the state for these must be placed in the dependency array so the value is immediately updated once state is set (calling the setter schedules an update, but doesn't immediately update the state value)
-    }, [currentStageIndex, nextStageInfo, previousStageInfo]);
-    
-    const handleStageChange = async (stageInfo: loansDisplayDataInterface) => {
-      try {
-        //patch request to update the pipelineStage
-        await fetch(`/api/loans/${params.id}`, {
+      }, [currentStageIndex, nextStageInfo, previousStageInfo]);
+      
+      const handleStageChange = async (stageInfo: loansDisplayDataInterface) => {
+        try {
+          //patch request to update the pipelineStage
+          await fetch(`/api/loans/${params.id}`, {
             method: "PATCH",
             headers: {
-                "Content-Type": "application/json",
+              "Content-Type": "application/json",
             },
             body: JSON.stringify({ pipelineStage: stageInfo.value }),
-        });
-        
-        // await fetch(`/api/activitylog`, {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //         loanId: params.id,
-        //         userId: user.id,
-        //         message: `${user.name} moved loan to ${stageInfo.value} stage.`,
-        //     }),
-        // });
-      } catch (error) {
-        console.error("ERROR: " + error)
-      }
-        //refresh the page to reflect the change
-        window.location.reload();
-    };
-    
-    const handleDelete = async () => {
-        try {
-            fetch(`/api/loans/${params.id}`, {
-                method: "DELETE",
-                headers: {
+          });
+          
+          // await fetch(`/api/activitylog`, {
+            //     method: "POST",
+            //     headers: {
+              //         "Content-Type": "application/json",
+              //     },
+              //     body: JSON.stringify({
+                //         loanId: params.id,
+                //         userId: user.id,
+                //         message: `${user.name} moved loan to ${stageInfo.value} stage.`,
+                //     }),
+                // });
+              } catch (error) {
+                console.error("ERROR: " + error)
+              }
+              //refresh the page to reflect the change
+              window.location.reload();
+            };
+            
+            const handleDelete = async () => {
+              try {
+                fetch(`/api/loans/${params.id}`, {
+                  method: "DELETE",
+                  headers: {
                     "Content-Type": "application/json",
-                },
-            });
-            window.location.href = "/loans/pipeline";
-        } catch {
+                  },
+                });
+                window.location.href = "/loans/pipeline";
+              } catch {
             console.error("An error occurred");
-        }
+          }
     };
     //this has been moved to just above the return statement because if not, the useState/useEffect hooks may not always call (ie if a loan is not found) which violates the rules of hooks - they must be called in the same order each time. 
     if (!loan) {
       return <ErrorMessage>Could not find loan.</ErrorMessage>;
     }
+    // this must also be moved down here for same reasons as line 126.
+    if (loan && 'message' in loan && loan.message === 'User is not a member of the team associated with this loan') {
+      return <ErrorMessage>{loan.message}</ErrorMessage>
+    }
     return (
-        <Box>
+      <Box>
       <LoanTabs params={params} isLoanActions={true} />
       <Card className="mt-5 !bg-darkGrey">
         <Flex gap={"3"} direction={"column"} className="animate-dropInLite">
